045812d5c6a17e24af1404959122420f49c533f3
Yudanto Anas <yudantoanas@hotmail.com>
2018-09-03 22:06:04 +0700

    first commit

---
+++ app/src/main/java/com/ceolabs/pariwisatabandung/CustomOnClickListener.java
@@ -0,0 +1,20 @@
+package com.ceolabs.pariwisatabandung;
+
+import android.view.View;
+
+class CustomOnItemClickListener implements View.OnClickListener {
+    private int position;
+    private OnItemClickCallback onItemClickCallback;
+    public CustomOnItemClickListener(int position, OnItemClickCallback onItemClickCallback) {
+        this.position = position;
+        this.onItemClickCallback = onItemClickCallback;
+    }
+
+    @Override
+    public void onClick(View view) {
+        onItemClickCallback.onItemClicked(view, position);
+    }
+    public interface OnItemClickCallback {
+        void onItemClicked(View view, int position);
+    }
+}
